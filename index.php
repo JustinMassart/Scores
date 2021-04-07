1172ad90ce4d2c853c604495d50ba480569b36e6
JustinMassart <justin.massart@student.hepl.be>
2021-04-07 14:36:55 +0200

    update matches section with dynamic datas

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
 
-include('vue.php');
+define('FILE_PATH', 'matches.csv');
+
+$matches = [];
+
+$handle = fopen(FILE_PATH, 'r');
+$headers = fgetcsv($handle, 1000);
+
+while($line = fgetcsv($handle, 1000)){
+    $matches[] = array_combine($headers, $line);
+}
+
+require('vue.php');
